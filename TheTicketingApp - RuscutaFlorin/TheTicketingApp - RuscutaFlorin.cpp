cdf45294fd8575c0b1c0c1ca39d35ff03ee172d2
Florin Ruscuta <ruscutaf@gmail.com>
2023-01-19 10:44:01 +0200

    improved features

--- TheTicketingApp - RuscutaFlorin/TheTicketingApp - RuscutaFlorin.cpp
+++ TheTicketingApp - RuscutaFlorin/TheTicketingApp - RuscutaFlorin.cpp
@@ -17,68 +17,53 @@ void RegisterEvent(Tickets& data) {
 	cout << data;
 }
 
-void BuyTicket(Tickets& data, SaleTickets& info) {
-	cin >> info;
-	if (info.isEligibleForBuying() && info.getID() != "Undefined") {
-		string* types = data.getTypes();
-		int* distribution = data.getTicketDistribution();
-		int* prices = data.getPrice();
-		for (int i = 0; i < data.getTypesNumber(); ++i) {
-			cout << i + 1 << ", Type: " << types[i] << ", Price: " << prices[i] << ", Distribution: " << distribution[i] << endl;
-			info.setTypesPriceAndDistribution(types, prices, distribution, data.getTypesNumber());
-		}
-		cout << "Type the id of the ticket you wanna buy!";
-		int id;
-		cin >> id;
-		while (id != 0) {
+void BuyTicket(Tickets data, SaleTickets& info) {
+	if (data.getTypes() != nullptr) {
+		cin >> info;
+		if (info.isEligibleForBuying() && info.getID() != "Undefined") {
+			string* types = data.getTypes();
+			int* distribution = data.getTicketDistribution();
+			int* prices = data.getPrice();
 			for (int i = 0; i < data.getTypesNumber(); ++i) {
-				if (i == id - 1) {
-					if (info.isTicketAvailable(types[i])) {
-						info.incrementallTimeSoldTickets(1);
-						cout << "You bought 1 ticket!" << " - " << info.getAllTimeSoldTickets() << endl;
-						distribution[i]--;
-						info.setTypesPriceAndDistribution(types, prices, distribution, data.getTypesNumber());
-					}
-					else {
-						cout << "Tickets of this type are not available, try again later!";
-						return;
+				cout << i + 1 << ", Type: " << types[i] << ", Price: " << prices[i] << ", Distribution: " << distribution[i] << endl;
+				info.setTypesPriceAndDistribution(types, prices, distribution, data.getTypesNumber());
+			}
+			cout << "Type the id of the ticket you wanna buy!";
+			int id;
+			cin >> id;
+			while (id != 0) {
+				for (int i = 0; i < data.getTypesNumber(); ++i) {
+					if (i == id - 1) {
+						if (info.isTicketAvailable(types[i])) {
+							info.incrementallTimeSoldTickets(1);
+							cout << "You bought 1 ticket!" << " - " << info.getAllTimeSoldTickets() << endl;
+							distribution[i]--;
+							info.setTypesPriceAndDistribution(types, prices, distribution, data.getTypesNumber());
+						}
+						else {
+							system("cls");
+							cout << "Tickets of this type are not available, try again later!";
+							return;
+						}
 					}
 				}
+				cout << "If you wanna stop, type 0, if you wanna buy more, type the ticket id: ";
+				cin >> id;
 			}
-			cout << "If you wanna stop, type 0, if you wanna buy more, type the ticket id: ";
-			cin >> id;
-		}
 
+		}
+		else {
+			cout << endl << "You're too young or you entered a incorect ID!";
+		}
 	}
 	else {
-		cout << endl << "You have to grow a little bit more!";
+		cout << "There is no event to register!";
+		return;
 	}
 }
 
 int main()
 {
-	/*buildingStructure a;
-	Location b;
-	Event c;
-	string asd[] = { "normal", "vip", "test" };
-	int x[4];
-	x[0] = 100;
-	x[1] = 200;
-	x[2] = 300;
-
-	Tickets d(3, asd, x, x, c);
-	SaleTickets e;
-	cout << endl << a;
-	cout << endl << b;
-	cout << endl << c;
-	cout << endl << d;
-	cout << endl << e;
-
-	int* x1 = (int*)d;
-	cout << d.getTypesNumber() << endl;
-	for (int i = 0; i < d.getTypesNumber(); ++i)
-		cout << x1[i] << endl;*/
-
 	cout << "Bun venit in cadrul aplicatiei de bilete!";
 	cout << endl << "Tastati 1 pentru inregistrarea unui EVENIMENT \nTastati 2 pentru cumpararea unui bilet!\n";
 	int operatie;
